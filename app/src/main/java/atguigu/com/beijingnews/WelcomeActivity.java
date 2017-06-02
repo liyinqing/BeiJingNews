e737e229f2389c4bb755d92fced7c7dd2ec878d4
liyinqing <yinqing_521@126.com>
2017-06-02 10:50:32 +0800

    Initial commit

---
+++ app/src/main/java/atguigu/com/beijingnews/WelcomeActivity.java
@@ -0,0 +1,61 @@
+package atguigu.com.beijingnews;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.RelativeLayout;
+
+import atguigu.com.beijingnews.activity.MainActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    RelativeLayout rl_welcome;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        rl_welcome = (RelativeLayout)findViewById(R.id.rl_welcome);
+
+        AlphaAnimation aa = new AlphaAnimation(0,1);
+        aa.setDuration(2000);
+        aa.setFillAfter(true);
+
+        RotateAnimation ra = new RotateAnimation(0,360, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        ra.setDuration(2000);
+        ra.setFillAfter(true);
+
+        ScaleAnimation sa = new ScaleAnimation(0,1,0,1,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        sa.setDuration(2000);
+        sa.setFillAfter(true);
+
+        AnimationSet set = new AnimationSet(false);
+        set.addAnimation(aa);
+        set.addAnimation(ra);
+        set.addAnimation(sa);
+
+        rl_welcome.startAnimation(set);
+
+        set.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(WelcomeActivity.this,MainActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
